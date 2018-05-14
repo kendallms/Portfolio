c75a5f4bca93db8aeb19c4473fe790975187f81d
Kendall MS <kendallms@users.noreply.github.com>
2018-05-14 15:34:33 -0400

    First commit with parcel and working nav buttons

---
+++ index.js
@@ -0,0 +1,27 @@
+// $('#work-nav').click(function(){
+//     $('.about-page').scrollTo('li:eq(15)', 2500)
+// })
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+
+    $(function () {
+        $('.lazy').Lazy();
+    });
+    $('#hello-nav').click(function (event) {
+        // event.preventDefault();
+        var goTop = $('#about-page').offset().top;
+        $("html, body").scrollTop(goTop, { duration: 300 });
+    })
+    $('#work-nav').click(function (event) {
+        // event.preventDefault();
+        var goTop = $('#work-page').offset().top;
+        $("html, body").scrollTop(goTop);
+    })
+    $('#contact-nav').click(function (event) {
+        // event.preventDefault();
+        var goTop = $('#contact-page').offset().top;
+        $("html, body").scrollTop(goTop);
+    })
+
+})
